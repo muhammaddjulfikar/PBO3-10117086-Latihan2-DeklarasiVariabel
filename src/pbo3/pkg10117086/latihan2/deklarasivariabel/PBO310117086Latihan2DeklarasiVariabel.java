2d2024b781644db9b10f4488263794101cb946fe
djulfikar <muhammaddjulfikar7@gmail.com>
2018-09-23 22:03:34 -0700

    DeklarasiVariabel

---
+++ src/pbo3/pkg10117086/latihan2/deklarasivariabel/PBO310117086Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ 
+ */
+package pbo3.pkg10117086.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author Lenovo
+ *  Nama     : RD Muhammad Djulfikar BU
+ *  NIM      : 10117086
+ *  Kelas    : IF3
+ *  Minggu 23 september 2018
+ */
+public class PBO310117086Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // deklarasi variabel
+       int nilaiInt;
+       final double PHI = 3.14; //konstanta uppercase
+       boolean nilaiLogika;
+       char nilaiKarakter;
+       
+       //memberi nilai ke variabel
+       nilaiInt = 78;
+       nilaiLogika = false;
+       nilaiKarakter = 'D';
+       
+       //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+        
+
+        
+        
+    }
+    
+}
